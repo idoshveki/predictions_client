c53f13f952b4efd6841059a81cb2c40b0ce584de
elad <eladfridman6@gmail.com>
2018-03-10 13:52:28 +0200

    init project wohoo

---
+++ prediction/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  componentDidMount() {
+  const config = {
+    apiKey: "AIzaSyBWcmGSK0NoszHLIJIitL3WWCBkBNHFJns",
+    authDomain: "prediction-39c2c.firebaseapp.com",
+    databaseURL: "https://prediction-39c2c.firebaseio.com",
+    projectId: "prediction-39c2c",
+    storageBucket: "prediction-39c2c.appspot.com",
+    messagingSenderId: "1073719092846"
+};
+firebase.initializeApp(config);
+}
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
